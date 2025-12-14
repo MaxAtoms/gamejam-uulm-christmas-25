692c50b140af84e0d8a8baf72b292104d7a4ee34
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2025-12-14 10:33:31 +0100

    fix projectile node type and collisions

--- projectile.gd
+++ projectile.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends Area2D
 
 @export var speed = 100
 
@@ -11,15 +11,9 @@ func _ready():
 	global_position = spawnPos
 	global_rotation = spawnRot
 
-func _physics_process(_delta: float) -> void:
-	velocity = Vector2(0, speed).rotated(dir)
-	move_and_slide()
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("enemy"):
-		body.get_node("Damagable").damage(damage)
-		queue_free()
-
+func _physics_process(delta: float) -> void:
+	var movement = Vector2(0,speed) * delta
+	position += movement
 
 func _on_lifetime_timeout() -> void:
 	pass
@@ -27,3 +21,11 @@ func _on_lifetime_timeout() -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print(body.name)
+	if body.is_in_group("enemy"):
+		print("Hit enemy!")
+		body.get_node("Damagable").damage(damage)
+		queue_free()
